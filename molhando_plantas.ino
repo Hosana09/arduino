a0989d8ed80d6856759a9e02901071bde495a0a9
Hosana09 <hosanapires9@gmail.com>
2024-01-22 20:27:06 -0300

    Pequenas alterações para melhor compreensão dos códigos.

--- molhando_plantas.ino
+++ molhando_plantas.ino
@@ -6,21 +6,16 @@ int rele = 2;
 #include "DHT.h"
 DHT dht(DHTPIN, DHTTYPE);
 
-// the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(rele, OUTPUT);
   dht.begin();
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
   int sensorValue = analogRead(umidadeSolo);
-  // print out the value you read:
   Serial.println(sensorValue);
-  delay(1000);  // delay in between reads for stability
+  delay(1000);
 
   if (sensorValue > 800){
     Serial.println("Solo seco");
@@ -32,20 +27,15 @@ void loop() {
   }
 
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
-  // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
   float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
   Serial.print(F("Humidity: "));
@@ -59,5 +49,4 @@ void loop() {
   Serial.print(F("°C "));
   Serial.print(hif);
   Serial.println(F("°F"));
-
 }
